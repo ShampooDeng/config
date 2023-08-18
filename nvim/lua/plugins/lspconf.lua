78d83b4dfd436d4ddb76552af5bd8f8ad94e1803
ShampooDeng <lwxkkdy@foxmail.com>
2023-08-18 10:25:02 +0800

    Add keymaps for leap.lua and lspconf.lua
    1.In leap.lua: gs -> \s for inter-windows leap
    2.In lsconf.lua: gs -> ctrl-' for signature help

--- nvim/lua/plugins/lspconf.lua
+++ nvim/lua/plugins/lspconf.lua
@@ -24,15 +24,29 @@ local function lsp_zero_config()
 	require("plugins.autocompletion").config()
 
 	-- Lsp keybindings:
-	-- K
-	-- gd
-	-- gD
-	-- gr
-	-- <F2>
-	-- <F3>
-	-- [d
-	-- ]d
 	-- Note: More keybindings related to lsp can be found in https://github.com/VonHeikemen/lsp-zero.nvim/blob/v2.x/doc/md/lsp.md
+	vim.api.nvim_create_autocmd('LspAttach', {
+		desc = 'LSP actions',
+		callback = function(event)
+		local opts = {buffer = event.buf}
+			vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+			vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+			vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+			vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+			vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+			vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+			vim.keymap.set({'n','i'}, "<C-'>", '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+			-- vim.keymap.set({'n','i'}, 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+			vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+			vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+			vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+
+			vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+			vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
+			vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts) 
+		end
+	})
+
 end
 
 
@@ -57,3 +71,4 @@ return {
 	},
 	config = lsp_zero_config
 }
+
