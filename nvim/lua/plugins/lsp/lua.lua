cad7359fadda5bbc96c1174c3f57167a4718d3fd
ShampooDeng <lwxkkdy@foxmail.com>
2023-08-19 17:34:10 +0800

    Update lua lsp config

--- nvim/lua/plugins/lsp/lua.lua
+++ nvim/lua/plugins/lsp/lua.lua
@@ -3,13 +3,26 @@ return {
 		settings = {
 			Lua = {
 				runtime = {
-					version = "LuaJIT",
+					-- version = "LuaJIT",
+					version = "Lua 5.4",
+					path = {
+						"?.lua",
+						"?/init.lua",
+						vim.fn.expand("~/.luarocks/share/lua/5.4/?/?.lua"),
+						vim.fn.expand("~/.luarocks/share/lua/5.4/?/init.lua"),
+						"/usr/share/lua/5.4/?.lua",
+						"/usr/share/lua/5.4/?/init.lua",
+					}
 				},
 				diagnostics = {
 					globals = {"vim"}
 				},
 				workspace = {
 					checkThirdParty = false,
+					library = {
+						vim.fn.expand("~/.luarocks/share/lua/5.4"),
+						"/usr/share/lua/5.4"
+					}
 				},
 				completion = {
 					enable = true,
